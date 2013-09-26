c780bab9a784ffe31653cb7d981fefedaefd52bf
Augusto Men <augusto.men@pdg.com.br>
2013-09-26 16:33:26 -0300

    Remove breaking import

--- simpledataimporter/__init__.py
+++ simpledataimporter/__init__.py
@@ -1,10 +1,5 @@
 # coding: utf-8
 
-# for importing: from dataimporter2 import SlugXLSXReader
-from csv import DictReader
-from readers.csv import SlugDictReader
-from readers.excel import ExcelReader, SlugExcelReader
-
 
 VERSION = (0, 1, 0, 'beta')
 if VERSION[-1] != "final":  # pragma: no cover
