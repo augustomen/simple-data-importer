fba622c436183e82f18f605b3f0dc211f2c0da96
augustomen <augustocmen@yahoo.com.br>
2014-06-11 12:31:25 -0300

    Adding excel_str to default import on simpledataimporter.readers

--- simpledataimporter/readers/__init__.py
+++ simpledataimporter/readers/__init__.py
@@ -1,3 +1,3 @@
 from .csv_reader import DictReader, SlugDictReader
-from .excel_reader import ExcelReader, SlugExcelReader
+from .excel_reader import ExcelReader, SlugExcelReader, excel_str
 from .xlsx_reader import XLSXReader, SlugXLSXReader
